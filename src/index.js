c7c96abb8a61fe409a8fd407f5e6b35e74f79c74
adamferrick <adameferrick@gmail.com>
2023-10-10 01:49:18 -0700

    add basic board ui

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './style.css';
 
-console.log('Hello world');
+document.querySelectorAll('.square').forEach(e => {
+  e.onclick = () => {
+    e.classList.remove('unknown');
+    if (Math.random() > 0.5) {
+      e.classList.add('hit');
+    } else {
+      e.classList.add('miss');
+    }
+  }
+});
