2cce11472ddb81e53da0e82e1fe987ebed10e7a2
adamferrick <adameferrick@gmail.com>
2023-10-15 23:58:34 -0700

    make board public

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -7,7 +7,7 @@ const _Square = () => {
 
 const Gameboard = (size = 10) => {
   const _ships = [];
-  const _board = new Array(size * size).fill().map(e => _Square());
+  const board = new Array(size * size).fill().map(e => _Square());
 
   function placeShip(length, x, y, vertical = true) {
     const ship = Ship(length)
@@ -18,25 +18,25 @@ const Gameboard = (size = 10) => {
         vertical ? y + offset : y,
         size,
       );
-      _board[idx].ship = ship;
+      board[idx].ship = ship;
     }
   }
 
   function receiveAttack(x, y) {
     const idx = xyToIdx(x, y, size)
-    if (_board[idx].receivedAttack) {
+    if (board[idx].receivedAttack) {
       throw new Error(`${x}, ${y} has already been attacked`);
-    } else if (_board[idx].ship !== null) {
-      _board[idx].ship.hit();
+    } else if (board[idx].ship !== null) {
+      board[idx].ship.hit();
     }
-    _board[idx].receivedAttack = true;
+    board[idx].receivedAttack = true;
   }
 
   function allShipsSunk() {
     return _ships.every(e => e.isSunk());
   }
 
-  return { placeShip, receiveAttack, allShipsSunk };
+  return { board, placeShip, receiveAttack, allShipsSunk };
 }
 
 module.exports = Gameboard;
