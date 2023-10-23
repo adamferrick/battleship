178c3b346876206b49a106be0978dfb872260f97
adamferrick <adameferrick@gmail.com>
2023-10-23 01:41:10 -0700

    default size arguments

--- src/coords.js
+++ src/coords.js
@@ -1,11 +1,11 @@
-function xyToIdx(x, y, size) {
+function xyToIdx(x, y, size = 10) {
   if (x >= size || y >= size || x < 0 || y < 0) {
     throw new Error(`x: ${x}, y: ${y}, size: ${size} is out of bounds`);
   }
   return y * size + x;
 }
 
-function idxToXy(idx, size) {
+function idxToXy(idx, size = 10) {
   if (idx >= size * size || idx < 0) {
     throw new Error(`idx: ${idx}, size: ${size} is out of bounds`);
   }
