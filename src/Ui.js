a0fb8d5e1490b8b28ce5edd550fa590d19fbeee7
adamferrick <adameferrick@gmail.com>
2023-10-23 01:43:08 -0700

    add functionality to UI

--- src/Ui.js
+++ src/Ui.js
@@ -1,27 +1,55 @@
-function _square() {
+const { xyToIdx, idxToXy } = require('./coords');
+
+function _square(attackCb) {
   const square = document.createElement('div');
   square.classList = 'square unknown';
   square.appendChild(document.createElement('div'));
   square.appendChild(document.createElement('div'));
+  if (attackCb !== undefined) {
+    square.onclick = attackCb;
+  }
   return square;
 }
 
-const Ui = (player1GridSel, player2GridSel, boardSize = 10) => {
-  const player1Grid = document.querySelector(player1GridSel);
-  const player2Grid = document.querySelector(player2GridSel);
+/*
+* player1 and player2 are objects with fields 'sel' and 'board', and 'actor'.
+* 'sel' is a selector for the player's own grid.
+* 'board' is the 'board' field for the player's Gameboard object.
+* 'actor' is the Player object associated with that player
+*/
+const ui = (player1, player2, boardSize = 10) => {
+  const player1Grid = document.querySelector(player1.sel);
+  const player2Grid = document.querySelector(player2.sel);
 
   for (let i = 0; i < boardSize * boardSize; i++) {
     const player1Square = _square();
-    const player2Square = _square();
+    const player2Square = _square(() => {
+      // player 1 has attacked this square
+      const coords = idxToXy(i);
+      player1.actor.attackSquare(coords.x, coords.y);
+      _updateGrid(player2Grid, player2.board, boardSize);
+      // TODO: check if player 2 is sunk
+      // player 2 should retaliate
+      player2.actor.attack();
+      _updateGrid(player1Grid, player1.board, boardSize);
+      // TODO: check if player 1 is sunk
+    });
     player1Grid.appendChild(player1Square);
     player2Grid.appendChild(player2Square);
   }
-
-  function updateGrids(player1, player2) {
-    // TODO
-  }
-
-  return { updateGrids };
 }
 
-module.exports = Ui;
+function _updateGrid(grid, board, boardSize = 10) {
+  for (let i = 0; i < boardSize * boardSize; i++) {
+    if (board[i].receivedAttack) {
+      grid.children[i].classList.remove('unknown');
+      if (board[i].ship !== null) {
+        grid.children[i].classList.add('hit');
+      } else {
+        grid.children[i].classList.add('miss');
+      }
+    }
+  }
+}
+
+module.exports = ui;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import './style.css';
-import Ui from './Ui.js';
+import ui from './Ui.js';
+import Gameboard from './Gameboard';
+import { Player, AiPlayer } from './Player';
 
-const ui = Ui('#player1 .grid', '#player2 .grid');
+const player1Gameboard = Gameboard();
+player1Gameboard.placeShip(1, 0, 0);
+const player2Gameboard = Gameboard();
+player2Gameboard.placeShip(1, 0, 0);
 
-document.querySelectorAll('.square').forEach(e => {
-  e.onclick = () => {
-    e.classList.remove('unknown');
-    if (Math.random() > 0.5) {
-      e.classList.add('hit');
-    } else {
-      e.classList.add('miss');
-    }
-  }
-});
+player1 = Player(player2Gameboard);
+player2 = AiPlayer(player1Gameboard);
+
+ui(
+  { sel: '#player1 .grid', board: player1Gameboard.board, actor: player1 },
+  { sel: '#player2 .grid', board: player2Gameboard.board, actor: player2 },
+);
