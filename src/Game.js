d72cc17c4cb4328686dbd7a1c04df71cc7fb0295
adamferrick <adameferrick@gmail.com>
2023-10-30 00:31:06 -0700

    do not process attacks after the game is over

--- src/Game.js
+++ src/Game.js
@@ -11,15 +11,28 @@ const Game = (size = 10) => {
   const _player1 = Player(_player2Gameboard);
   const _player2 = AiPlayer(_player1Gameboard);
 
+  let _gameOver = false;
+
   function update() {
     _ui.updateGrids(_player1Gameboard.board, _player2Gameboard.board);
+    if (_player2Gameboard.allShipsSunk()) {
+      _gameOver = true;
+      console.log('Player 1 wins!');
+    } else if (_player1Gameboard.allShipsSunk()) {
+      _gameOver = true;
+      console.log('Player 2 wins!');
+    }
   }
 
   function _attackSelected(x, y) {
-    _player1.attackSquare(x, y);
-    update();
-    _player2.attack();
-    update();
+    if (!_gameOver) {
+      _player1.attackSquare(x, y);
+      update();
+    }
+    if (!_gameOver) {
+      _player2.attack();
+      update();
+    }
   }
 
   const _ui = Ui('#player1 .grid', '#player2 .grid', _attackSelected, size);
